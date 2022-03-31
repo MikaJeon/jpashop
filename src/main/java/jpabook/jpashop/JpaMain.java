540b9f01da7ce72a894eebf937230a1d037f2774
MikaJeon <wjstngus1031@gmail.com>
2022-04-01 06:26:15 +0900

    1. 데이터 중심 JPA 설계

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //로딩 시점에 딱 하나만 만들어줘야 한다.
+        EntityManager em = emf.createEntityManager();
+        //db커넥션 얻어서 쿼리 날리고 종료 하는 단위마다 엔티티매니저 만들어줘야 함
+
+        EntityTransaction tx = em.getTransaction();//데이터베이스 트랜잭션 생성
+        tx.begin();//tx시작.
+        //트랜잭션이란?  데이터베이스의 상태를 변화시키기 해서 수행하는 작업의 단위
+
+        try{
+            tx.commit();//트랜젝션의 처리 과정을 데이터베이스에 반영하기 위해서, 변경된 내용을 모두 영구 저장. + 트랜잭션 종료
+        }
+        catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private int price;
+    private String name;
+    private int stockQuantity;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
